1342e8b24d9ba2b9450d9ee967f942ceb409152c
Chris Jones <chrisjonesdesign85@gmail.com>
2019-07-11 04:11:26 -0400

    Added some font awesome on the side bar and changed colors[B to grey for now.

--- sleep-tracker/src/components/Stats.js
+++ sleep-tracker/src/components/Stats.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import FontAwesomeIcon from 'react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import StatStyles from './StatStyles.css';
 
 
@@ -9,18 +7,40 @@ const Stats = props => {
         <div className="stats-container">
             <div className="sidebar">
                 <div className="icon-box">
-                    <FontAwesomeIcon icon={faCoffee} />
+                    <a href="#">
+                        <i class="fas fa-cloud-moon fa-3x statAwesome"></i>
+                    </a>
                 </div>
+                <div className="icon-box">
+                    <i class="fas fa-chart-area fa-3x statAwesome"></i>
+                </div>
+                <div className="icon-box">
+                    <i class="fas fa-pencil-alt fa-3x statAwesome"></i>
+                </div>
+                <div className="icon-box">
+                    <a href="#">
+                        <i className="fas fa-trash fa-3x statAwesome"></i>
+                    </a>
+                </div>
+                <div className="icon-box">
+                    <a href="#">
+                        <i class="fas fa-cog fa-3x statAwesome"></i>
+                    </a>
+                </div>
+
             </div>
             <div className="stats-main-container">
                 <div className="stats-main">
-                    <h1>Sleep Tracker App</h1>
+                    <div className="stats-nav">
+                        <h1>HYPNOS</h1>
+                        <i className="fas fa-bars fa-3x statAwesome"></i>
+                    </div>
                 </div>
                 <div className="stats-bottom-section">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
